1016cd85da1730c3c3a7ac025eb7ff9ea935b5de
Arnau Abella <arnauabella@gmail.com>
2020-11-17 17:50:35 +0100

    jacob method finished

--- parallelism/lab/assignment1/openmp/heat.h
+++ parallelism/lab/assignment1/openmp/heat.h
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <omp.h>
 
 // configuration
 
--- parallelism/lab/assignment1/openmp/solver-omp.c
+++ parallelism/lab/assignment1/openmp/solver-omp.c
@@ -9,26 +9,35 @@
  */
 double relax_jacobi (double *u, double *utmp, unsigned sizex, unsigned sizey)
 {
+    /*
+     * Traverse the matrix  of size (sizex, sizey)
+     * At each position,
+     *    compute the average of the neighbors i.e. left, right, top, bottom.
+     *    compute the difference with the average.
+     *    sum the diff^2
+     */
     double diff, sum=0.0;
     int nbx, bx, nby, by;
-  
-    nbx = NB;
-    bx = sizex/nbx;
-    nby = NB;
-    by = sizey/nby;
-    for (int ii=0; ii<nbx; ii++)
-        for (int jj=0; jj<nby; jj++) 
-            for (int i=1+ii*bx; i<=min((ii+1)*bx, sizex-2); i++) 
-                for (int j=1+jj*by; j<=min((jj+1)*by, sizey-2); j++) {
-	            utmp[i*sizey+j]= 0.25 * (u[ i*sizey     + (j-1) ]+  // left
-					     u[ i*sizey     + (j+1) ]+  // right
-				             u[ (i-1)*sizey + j     ]+  // top
-				             u[ (i+1)*sizey + j     ]); // bottom
-	            diff = utmp[i*sizey+j] - u[i*sizey + j];
-	            sum += diff * diff; 
-	        }
 
+    nbx = omp_get_max_threads();
+    bx = (sizex + nbx - 1)/nbx;
+    nby = omp_get_max_threads();
+    by = (sizey + nby - 1)/nby;
+
+    #pragma omp parallel for private(diff) reduction(+:sum) collapse(2)
+    for (int ii=0; ii<nbx; ii++)
+        for (int jj=0; jj<nby; jj++)
+            for (int i=1+ii*bx; i<=min((ii+1)*bx, sizex-2); i++)
+                for (int j=1+jj*by; j<=min((jj+1)*by, sizey-2); j++) {
+                  utmp[i*sizey+j] = 0.25 * (u[ i*sizey     + (j-1) ]+  // left
+                                            u[ i*sizey     + (j+1) ]+  // right
+                                            u[ (i-1)*sizey + j     ]+  // top
+                                            u[ (i+1)*sizey + j     ]); // bottom
+                  diff = utmp[i*sizey+j] - u[i*sizey + j];
+                  sum += diff * diff;
+                }
     return sum;
+
 }
 
 /*
@@ -50,14 +59,14 @@ double relax_redblack (double *u, unsigned sizex, unsigned sizey)
         for (int jj=lsw; jj<nby; jj=jj+2) 
             for (int i=1+ii*bx; i<=min((ii+1)*bx, sizex-2); i++) 
                 for (int j=1+jj*by; j<=min((jj+1)*by, sizey-2); j++) {
-	            unew= 0.25 * (    u[ i*sizey	+ (j-1) ]+  // left
-				      u[ i*sizey	+ (j+1) ]+  // right
-				      u[ (i-1)*sizey	+ j     ]+  // top
-				      u[ (i+1)*sizey	+ j     ]); // bottom
-	            diff = unew - u[i*sizey+ j];
-	            sum += diff * diff; 
-	            u[i*sizey+j]=unew;
-	        }
+                    unew= 0.25 * ( u[ i*sizey     + (j-1) ]+  // left
+                                   u[ i*sizey     + (j+1) ]+  // right
+                                   u[ (i-1)*sizey + j     ]+  // top
+                                   u[ (i+1)*sizey + j     ]); // bottom
+                    diff = unew - u[i*sizey+ j];
+                    sum += diff * diff;
+                    u[i*sizey+j] = unew;
+                }
     }
 
     // Computing "Black" blocks
@@ -66,14 +75,14 @@ double relax_redblack (double *u, unsigned sizex, unsigned sizey)
         for (int jj=lsw; jj<nby; jj=jj+2) 
             for (int i=1+ii*bx; i<=min((ii+1)*bx, sizex-2); i++) 
                 for (int j=1+jj*by; j<=min((jj+1)*by, sizey-2); j++) {
-	            unew= 0.25 * (    u[ i*sizey	+ (j-1) ]+  // left
-				      u[ i*sizey	+ (j+1) ]+  // right
-				      u[ (i-1)*sizey	+ j     ]+  // top
-				      u[ (i+1)*sizey	+ j     ]); // bottom
-	            diff = unew - u[i*sizey+ j];
-	            sum += diff * diff; 
-	            u[i*sizey+j]=unew;
-	        }
+                    unew= 0.25 * ( u[ i*sizey     + (j-1) ]+  // left
+                                   u[ i*sizey     + (j+1) ]+  // right
+                                   u[ (i-1)*sizey + j     ]+  // top
+                                   u[ (i+1)*sizey + j     ]); // bottom
+                    diff = unew - u[i*sizey+ j];
+                    sum += diff * diff;
+                    u[i*sizey+j] = unew;
+                }
     }
 
     return sum;
@@ -95,15 +104,13 @@ double relax_gauss (double *u, unsigned sizex, unsigned sizey)
         for (int jj=0; jj<nby; jj++) 
             for (int i=1+ii*bx; i<=min((ii+1)*bx, sizex-2); i++) 
                 for (int j=1+jj*by; j<=min((jj+1)*by, sizey-2); j++) {
-	            unew= 0.25 * (    u[ i*sizey	+ (j-1) ]+  // left
-				      u[ i*sizey	+ (j+1) ]+  // right
-				      u[ (i-1)*sizey	+ j     ]+  // top
-				      u[ (i+1)*sizey	+ j     ]); // bottom
-	            diff = unew - u[i*sizey+ j];
-	            sum += diff * diff; 
-	            u[i*sizey+j]=unew;
+                    unew= 0.25 * ( u[ i*sizey     + (j-1) ]+  // left
+                                   u[ i*sizey     + (j+1) ]+  // right
+                                   u[ (i-1)*sizey + j     ]+  // top
+                                   u[ (i+1)*sizey + j     ]); // bottom
+                    diff = unew - u[i*sizey+ j];
+                    sum += diff * diff;
+                    u[i*sizey+j]=unew;
                 }
-
     return sum;
 }
-
